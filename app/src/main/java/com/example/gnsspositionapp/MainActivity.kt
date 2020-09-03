12550ed82daa1d5cd777a51ec0aa4a348354ce32
yusaito3333 <yusaito3333@gmail.com>
2020-09-03 16:05:29 +0900

    add setUp toolbar

--- app/src/main/java/com/example/gnsspositionapp/MainActivity.kt
+++ app/src/main/java/com/example/gnsspositionapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.gnsspositionapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpToolbar()
+    }
+
+    private fun setUpToolbar() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        val appBarConf = AppBarConfiguration(navController.graph)
+
+        toolbar.setupWithNavController(navController,appBarConf)
     }
 }

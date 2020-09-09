2eb32c2d3683dbfb87dca0af729af4b0c01962cd
yusaito3333 <yusaito3333@gmail.com>
2020-09-09 13:47:30 +0900

    add process when the back button pressed

--- app/src/main/java/com/example/gnsspositionapp/ui/measure/MeasureFragment.kt
+++ app/src/main/java/com/example/gnsspositionapp/ui/measure/MeasureFragment.kt
@@ -4,38 +4,86 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.Navigation
 import com.example.gnsspositionapp.R
-import kotlinx.android.synthetic.main.loading_layout.view.*
-import kotlinx.android.synthetic.main.measure_finished_layout.view.*
-import kotlinx.android.synthetic.main.measure_fragment.*
-import kotlinx.android.synthetic.main.measure_start_layout.view.*
+import com.example.gnsspositionapp.databinding.MeasureFragmentBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class MeasureFragment : Fragment() {
+@AndroidEntryPoint
+class MeasureFragment : Fragment(),OnBackPressHandler {
+
+    private lateinit var adapter: LocationListAdapter
+
+    private lateinit var binding : MeasureFragmentBinding
+
+    private val measureViewModel : MeasureViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.measure_fragment,container,false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.measure_fragment,
+            container,
+            false
+        )
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        start_view.btn_start.setOnClickListener{
-            start_view.visibility = View.GONE
-            loading_view.visibility = View.VISIBLE
+        adapter = LocationListAdapter(measureViewModel.unitIsYard.value ?: false)
+
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+
+            finishedView.locationList.adapter = adapter
+
+            viewModel = measureViewModel
+
+            loadingView.btnEnd.setOnClickListener {
+                measureViewModel.finishMeasuringLocation()
+            }
+
+            finishedView.btnMeasureAgain.setOnClickListener {
+                measureViewModel.startMeasuringLocation()
+            }
+
+            finishedView.btnSave.setOnClickListener {
+                measureViewModel.saveLocations()
+            }
         }
-        loading_view.btn_end.setOnClickListener {
-            loading_view.visibility = View.GONE
-            finished_view.visibility = View.VISIBLE
+
+        measureViewModel.locations.observe(viewLifecycleOwner) {
+            adapter.submitList(it)
         }
-        finished_view.btn_measure_again.setOnClickListener {
-            finished_view.visibility = View.GONE
-            start_view.visibility = View.VISIBLE
+
+        measureViewModel.unitIsYard.observe(viewLifecycleOwner) {
+            adapter.convertUnit(it)
         }
+
     }
 
+    override fun onBackPressed() : Boolean{
+        return if(measureViewModel.state.value == MeasureViewModel.MeasureState.MEASURING){
+            AlertDialog.Builder(requireContext())
+                .setMessage(resources.getString(R.string.dialog_desc))
+                .setPositiveButton(resources.getString(R.string.dialog_positive)) { _, _ ->
+                    measureViewModel.finishMeasuringLocation()
+                    Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_container).popBackStack()
+                }
+                .setNegativeButton(resources.getString(R.string.dialog_negative)) { _, _ -> }
+                .show()
+            true
+        }else{
+            false
+        }
+    }
 }

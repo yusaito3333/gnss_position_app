bd8495aad5845e7ef3f081356069fbcb851df3d9
yusaito3333 <yusaito3333@gmail.com>
2020-09-09 13:39:51 +0900

    enable timber and hilt

--- app/src/main/java/com/example/gnsspositionapp/App.kt
+++ app/src/main/java/com/example/gnsspositionapp/App.kt
@@ -2,12 +2,19 @@ package com.example.gnsspositionapp
 
 import android.app.Application
 import com.jakewharton.threetenabp.AndroidThreeTen
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
+@HiltAndroidApp
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
         AndroidThreeTen.init(this)
+
+        if(BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }

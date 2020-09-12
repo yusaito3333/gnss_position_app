7dfe334fa8b1dd9c26369d374a082e29a4759104
yssai2011 <askpqts@gmail.com>
2020-09-12 14:34:23 +0900

    add file header

--- app/src/main/java/com/example/gnsspositionapp/usecase/measure/SaveLocationUseCase.kt
+++ app/src/main/java/com/example/gnsspositionapp/usecase/measure/SaveLocationUseCase.kt
@@ -1,25 +1,26 @@
 package com.example.gnsspositionapp.usecase.measure
 
-import com.example.gnsspositionapp.data.Result
 import android.content.Context
 import com.example.gnsspositionapp.data.LocationInfo
+import com.example.gnsspositionapp.data.Result
 import com.example.gnsspositionapp.usecase.BaseUseCase
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.flow
 import org.threeten.bp.LocalDate
-import org.threeten.bp.LocalDateTime
-import timber.log.Timber
 import java.io.*
 import java.nio.charset.Charset
 import java.util.*
 import javax.inject.Inject
-import kotlin.collections.ArrayDeque
 
 @Suppress("BlockingMethodInNonBlockingContext")
 class SaveLocationUseCase @Inject constructor(
     @ApplicationContext private val context : Context
 ) : BaseUseCase<Queue<LocationInfo>,Unit>(Dispatchers.IO){
+
+    companion object {
+        private const val HEADER = "位置名称,日時,緯度,経度,精度,高さ"
+    }
 
     override fun execute(parameters : Queue<LocationInfo>) = flow{
 
@@ -35,13 +36,23 @@ class SaveLocationUseCase @Inject constructor(
 
             val file = File(dir,"${date}.csv")
 
+            var hasHeader = true
+
             if(!file.exists()){
                 file.createNewFile()
+                hasHeader = false
             }
 
             BufferedWriter(OutputStreamWriter(FileOutputStream(file,true), Charset.forName("UTF-8")))
                 .use{
+
+                    if(!hasHeader){
+                        it.write(HEADER)
+                        it.newLine()
+                    }
+
                     it.write(locationInfo.toCSVFormat())
+                    it.newLine()
                 }
         }
         emit(Result.Success(Unit))

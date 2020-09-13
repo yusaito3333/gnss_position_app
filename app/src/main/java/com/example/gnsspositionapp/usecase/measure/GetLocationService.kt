d34019e790e20705807a5e9fbffecb05688ecf73
yssai2011 <askpqts@gmail.com>
2020-09-13 14:17:15 +0900

    inject instance change variable name

--- app/src/main/java/com/example/gnsspositionapp/usecase/measure/GetLocationService.kt
+++ app/src/main/java/com/example/gnsspositionapp/usecase/measure/GetLocationService.kt
@@ -4,14 +4,14 @@ import android.app.*
 import android.content.Intent
 import android.content.res.Configuration
 import android.location.Location
-import android.location.LocationManager
 import android.os.*
-import androidx.core.app.JobIntentService
 import androidx.core.app.NotificationCompat
 import com.example.gnsspositionapp.R
-import com.example.gnsspositionapp.data.Result
 import com.example.gnsspositionapp.ui.measure.LocationCallbackWrapper
-import com.google.android.gms.location.*
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -26,8 +26,6 @@ class GetLocationService : Service() {
     companion object {
         private const val PACKAGE_NAME = "com.example.gnsspositionapp.usecase.measure"
 
-        private const val BROAD_CAST = "$PACKAGE_NAME.broadcast"
-
         private val TAG = GetLocationService::class.simpleName!!
 
         private const val CHANNEL_ID = "location_channel"
@@ -41,7 +39,7 @@ class GetLocationService : Service() {
 
     private var mChangeConf = false
 
-    @Inject lateinit var repository: Repository
+    @Inject lateinit var repository: LocationRepository
 
     private val job = Job()
 
@@ -49,9 +47,9 @@ class GetLocationService : Service() {
 
     private lateinit var mNotificationManager : NotificationManager
 
-    private lateinit var mLocationRequest : LocationRequest
+    @Inject lateinit var mLocationRequest : LocationRequest
 
-    private lateinit var mFusedLocationClient : FusedLocationProviderClient
+    @Inject lateinit var mFusedLocationClient : FusedLocationProviderClient
 
     private lateinit var mLocationCallbackWrapper: LocationCallbackWrapper
 
@@ -59,22 +57,15 @@ class GetLocationService : Service() {
 
     override fun onCreate() {
 
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
-        mLocationCallbackWrapper = LocationCallbackWrapper(object : LocationCallback(){
-            override fun onLocationResult(p0: LocationResult?) {
-                super.onLocationResult(p0)
-                p0?.let{
-                    onNewLocation(p0.lastLocation)
+        mLocationCallbackWrapper = LocationCallbackWrapper(object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                super.onLocationResult(locationResult)
+                locationResult?.let {
+                    Timber.tag("locationService").d("${it.lastLocation.accuracy}")
+                    onNewLocation(locationResult.lastLocation)
                 }
             }
         })
-
-        mLocationRequest = LocationRequest().apply{
-            interval = 1000
-            fastestInterval = 1000
-            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        }
 
         val handlerThread = HandlerThread(TAG)
 
@@ -88,7 +79,11 @@ class GetLocationService : Service() {
 
             val name = getString(R.string.notification_channel_name)
 
-            val mChannel = NotificationChannel(CHANNEL_ID,name,NotificationManager.IMPORTANCE_DEFAULT)
+            val mChannel = NotificationChannel(
+                CHANNEL_ID,
+                name,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
 
             mNotificationManager.createNotificationChannel(mChannel)
         }
@@ -98,9 +93,10 @@ class GetLocationService : Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        val startedFromNotification = intent?.getBooleanExtra(EXTRA_STARTED_NOTIFICATION,false) ?: false
+        val startedFromNotification = intent?.getBooleanExtra(EXTRA_STARTED_NOTIFICATION, false) ?: false
 
         if(startedFromNotification){
+            Timber.d("stop service")
             removeLocationUpdates()
             stopSelf()
         }
@@ -109,30 +105,32 @@ class GetLocationService : Service() {
     }
 
     fun requestLocationUpdates() {
-        startService(Intent(applicationContext,GetLocationService::class.java))
+        startService(Intent(applicationContext, GetLocationService::class.java))
 
         try{
-            mFusedLocationClient.requestLocationUpdates(mLocationRequest,
-            mLocationCallbackWrapper,Looper.myLooper())
-        }catch (ex : SecurityException){
+            mFusedLocationClient.requestLocationUpdates(
+                mLocationRequest,
+                mLocationCallbackWrapper, Looper.myLooper()
+            )
+        }catch (ex: SecurityException){
             Timber.e("Lost location permission. $ex")
         }
     }
 
     fun removeLocationUpdates() {
 
-        stopService(Intent(applicationContext,GetLocationService::class.java))
+        stopService(Intent(applicationContext, GetLocationService::class.java))
 
         try{
             mFusedLocationClient.removeLocationUpdates(mLocationCallbackWrapper)
-        }catch (ex : SecurityException){
+        }catch (ex: SecurityException){
             Timber.e("Lost location permission. $ex")
         }
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
-
+        Timber.d("Configuration changed")
         mChangeConf = true
     }
 
@@ -142,7 +140,7 @@ class GetLocationService : Service() {
         }
     }
 
-    override fun onBind(p0: Intent?): IBinder? {
+    override fun onBind(intent: Intent?): IBinder? {
         stopForeground(true)
 
         mChangeConf = false
@@ -161,22 +159,35 @@ class GetLocationService : Service() {
     override fun onUnbind(intent: Intent?): Boolean {
 
         if(!mChangeConf){
-            startForeground(NOTIFICATION_ID,getNotification())
+            startForeground(NOTIFICATION_ID, getNotification())
         }
 
-        return super.onUnbind(intent)
+        return true
+    }
+
+    override fun onDestroy() {
+        mServiceHandler.removeCallbacksAndMessages(null)
     }
 
     private fun getNotification(): Notification {
 
-        val intent = Intent(this,GetLocationService::class.java)
+        val intent = Intent(this, GetLocationService::class.java)
 
-        intent.putExtra(EXTRA_STARTED_NOTIFICATION, true);
+        intent.putExtra(EXTRA_STARTED_NOTIFICATION, true)
 
-        val servicePendingIntent = PendingIntent.getService(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT)
+        val servicePendingIntent = PendingIntent.getService(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
 
         val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .addAction(R.drawable.ic_baseline_location_on_24,getString(R.string.notification_remove),servicePendingIntent)
+            .addAction(
+                R.drawable.ic_baseline_location_on_24,
+                getString(R.string.notification_remove),
+                servicePendingIntent
+            )
             .setContentText(getString(R.string.notification_content))
             .setContentTitle(getString(R.string.notification_title))
             .setOngoing(true)

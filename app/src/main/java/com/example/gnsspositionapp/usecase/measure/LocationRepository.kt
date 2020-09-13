a05503be997909bc45ed9169de6b885ab34ac244
yssai2011 <askpqts@gmail.com>
2020-09-13 14:19:43 +0900

    refactor code

--- app/src/main/java/com/example/gnsspositionapp/usecase/measure/LocationRepository.kt
+++ app/src/main/java/com/example/gnsspositionapp/usecase/measure/LocationRepository.kt
@@ -4,22 +4,24 @@ import android.location.Location
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.channels.BroadcastChannel
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.asFlow
+import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @Singleton
-class Repository
+class LocationRepository
     @Inject constructor(){
 
-    private val _channel = BroadcastChannel<Location>(10)
+    private val _channel = BroadcastChannel<Location>(Channel.BUFFERED)
 
     @OptIn(FlowPreview::class)
     val channel = _channel.asFlow()
 
     suspend fun addLocation(location: Location){
         _channel.send(location)
+        Timber.tag("locationRepository").d("${location.accuracy}")
     }
-
 }

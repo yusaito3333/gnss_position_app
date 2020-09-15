056e5d7f9847848c12bf05ee14e92bf7fa97c49c
yusaito3333 <yusaito3333@gmail.com>
2020-09-15 14:17:57 +0900

    clean

--- app/src/main/java/com/example/gnsspositionapp/usecase/send/LocationFileRepository.kt
+++ app/src/main/java/com/example/gnsspositionapp/usecase/send/LocationFileRepository.kt
@@ -1,12 +1,8 @@
 package com.example.gnsspositionapp.usecase.send
 
 import android.content.Context
-import android.net.Uri
-import com.example.gnsspositionapp.data.Result
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
-import timber.log.Timber
 import java.io.File
 import java.io.FileNotFoundException
 import java.io.IOException
@@ -29,8 +25,6 @@ class LocationFileRepository
             ?: throw FileNotFoundException("External Storage is not found")
 
         val files = dir.listFiles() ?: throw IOException("didn't get files")
-
-        Timber.d("$files")
 
         _filesFlow.value = files.toList()
     }

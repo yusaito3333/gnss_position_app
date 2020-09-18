1cb7b69d0630472ad03a92191964fd44f5eb6a28
yssai2011 <askpqts@gmail.com>
2020-09-18 09:15:31 +0900

    add bktree

--- app/src/main/java/com/example/gnsspositionapp/adapter/AutoCompleteAdapter.kt
+++ app/src/main/java/com/example/gnsspositionapp/adapter/AutoCompleteAdapter.kt
@@ -3,8 +3,10 @@ package com.example.gnsspositionapp.adapter
 import android.content.Context
 import android.widget.ArrayAdapter
 import android.widget.Filter
+import androidx.annotation.WorkerThread
 import java.util.regex.Pattern
 import javax.inject.Singleton
+import kotlin.math.min
 
 class AutoCompleteAdapter(context : Context, resource : Int,private val objects : List<String>)
     : ArrayAdapter<String>(context,resource,objects) {
@@ -58,16 +60,13 @@ class AutoCompleteAdapter(context : Context, resource : Int,private val objects 
 }
 
 @Singleton
-private class BKTree(dict : List<String>) {
+class BKTree {
 
     private lateinit var tree : Node
 
     class Node(val value : String , val children : MutableMap<Int,Node>)
 
-    init {
-
-    }
-
+    @WorkerThread
     fun add(node : Node){
         if(!::tree.isInitialized){
             tree = node
@@ -88,19 +87,61 @@ private class BKTree(dict : List<String>) {
         }
     }
 
-    private fun search(radius : Int,word : String) : List<String> {
+    @WorkerThread
+    fun search(radius : Int,word : String) : List<String> {
         val candidate = ArrayDeque<Node>()
         candidate.add(tree)
         val result = arrayListOf<String>()
 
-        while (true){
+        while (candidate.isNotEmpty()){
             val current = candidate.removeFirst()
 
+            val dist = dist(word,current.value)
+
+            if(dist <= radius){
+                result.add(current.value)
+            }
+
+            val low = dist-radius
+            val high = dist+radius
+
+            current.children.filter {
+                it.key in low..high
+            }.forEach { candidate.addLast(it.value) }
             
         }
+
+        return result
     }
 
-    private fun dist(s : String , t : String) : Int{
-        TODO()
+    fun dist(s : String , t : String) : Int{
+        val sLen = s.length + 1
+        val tLen = t.length + 1
+
+        var cost = Array(sLen){ it }
+
+        var newCost = Array(sLen){ 0 }
+
+        for (i in 1 until tLen){
+            newCost[0] = i
+
+            for( j in 1 until sLen){
+                val match = if(s[j-1] == t[i-1]) 0 else 1
+
+                val replaceCost = cost[j-1] + match
+                val insertCost = cost[j] + 1
+                val deleteCost = newCost[j-1] + 1
+
+                newCost[j] = min(min(replaceCost,insertCost),deleteCost)
+            }
+
+            val temp = cost
+
+            cost = newCost
+
+            newCost = temp
+        }
+
+        return cost[sLen-1]
     }
 }
--- app/src/test/java/com/example/gnsspositionapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/gnsspositionapp/ExampleUnitTest.kt
@@ -1,5 +1,9 @@
 package com.example.gnsspositionapp
 
+import com.example.gnsspositionapp.adapter.BKTree
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -10,8 +14,35 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    val data = listOf("some","soft","same","mole","soda","salmon")
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun addition_isCorrect() = runBlocking {
+
+        val tree = BKTree()
+
+        val job = launch(Dispatchers.Default) {
+            data.forEach { tree.add(BKTree.Node(it, mutableMapOf())) }
+        }
+
+        job.join()
+
+        val searchList = tree.search(2,"sort")
+
+        println(searchList)
+        assert(searchList.contains("soft"))
+        assert(searchList.contains("some"))
+        assert(searchList.contains("soda"))
+        assert(!searchList.contains("same"))
+        assert(!searchList.contains("mole"))
+        assert(!searchList.contains("salmon"))
+    }
+
+    @Test
+    fun distCorrect() {
+        val tree = BKTree()
+
+        assertEquals(tree.dist("some","soft"),2)
     }
 }
